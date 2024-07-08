99bd830a35f9082fd40538019589ad472ff3a33b
Simon Ball <simonball245@gmail.com>
2024-07-08 17:12:57 +0200

    Add react router and pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Explore from './pages/Explore';
+import ForgetPassword from './pages/ForgetPassword';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
 function App() {
-  return <div className="App">App works!</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgetPassword />} />
+        </Routes>
+      </Router>
+      {/* Navbar */}
+    </>
+  );
 }
 
 export default App;
