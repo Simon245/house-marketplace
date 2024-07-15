117d38f6f2d60e96426b8e29c277fa5a7c57988a
Simon Ball <simonball245@gmail.com>
2024-07-15 16:00:12 +0200

    Add an individual listing page and fetch data from firebase

---
+++ src/pages/Listing.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { getDoc, doc } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { db } from '../firebase.config';
+import Spinner from '../components/Spinner';
+import shareIcon from '../assets/svg/shareIcon.svg';
+
+function Listing(props) {
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [shareLinkedCopied, setShareLinkedCopied] = useState(null);
+
+  const navigate = useNavigate();
+  const params = useParams();
+  const auth = getAuth();
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      const docRef = doc(db, 'listings', params.listingId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        setListing(docSnap.data());
+        console.log(docSnap.data());
+        setLoading(false);
+      }
+    };
+    fetchListing();
+  }, [navigate, params.listingId]);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return listing && <div>Listing {listing.name}</div>;
+}
+
+export default Listing;
